f764a1da776291b83e829500726206b24446da0a
Xavier Dunkley <xavierxv18@gmail.com>
2022-08-11 19:22:27 -0400

    completed lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+
+  const [questions, setQuestions]= useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+      .then((res)=>res.json())
+      .then((data)=>setQuestions(data))
+    })
+
+    function handlePatch(id,correctIndex){
+      
+      fetch(`http://localhost:4000/questions/${id}`,{
+        method: "PATCH",
+        headers:{"Content-Type":"application/json"},
+        body: JSON.stringify({correctIndex})
+        
+      })  
+      .then((r)=>r.json())
+      .then((data)=>{
+        const matchQuestion= questions.map((question)=>{
+          if(data.id===question.id){
+              return (data)
+          }
+          return question
+        })
+        setQuestions(matchQuestion)
+      })
+    }
+  
+    
+    let questionItem=questions.map((question)=><QuestionItem key={question.id} question={question} setQuestions={setQuestions} questions={questions} handlePatch={handlePatch}/>)
+
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItem}</ul>
     </section>
   );
 }
